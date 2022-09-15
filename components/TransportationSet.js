553e0a354e8739bf19c9844de8f5819e41405ae2
cedjustin <cedro265@gmail.com>
2022-09-15 18:33:26 +0200

    added van and formatted currency

--- components/TransportationSet.js
+++ components/TransportationSet.js
@@ -3,6 +3,7 @@ import { MotiView } from 'moti'
 import React from 'react'
 import { Dimensions, FlatList, StyleSheet, View, Image } from 'react-native'
 import { default as theme } from '../theme/theme.json';
+import { formatCurrency, getSupportedCurrencies } from "react-native-format-currency";
 import transportData from '../data/transport-types.json'
 
 const itemSize = Dimensions.get('window').width / 3
@@ -14,13 +15,14 @@ const TransportationSet = (props) => {
     }
 
     const calculateDistanceCost = (cost)=>{
-        if(!props.locationSetOpen) return (props.distance/1000) * cost
+        const [valueFormattedWithSymbol, valueFormattedWithoutSymbol, symbol] = formatCurrency({ amount: Math.round((props.distance/1000) * cost), code: 'USD' })
+        if(!props.locationSetOpen) return valueFormattedWithoutSymbol
     }
 
     return (
         <MotiView
             style={styles.container}
-            from={{ top: -250 }}
+            from={{ top: 0 }}
             animate={{ top: props.startingPoint && props.destination ? -250 : 0 }}
             transition={{ type: 'timing', delay: 1000 }}
         >
@@ -47,7 +49,7 @@ const TransportationSet = (props) => {
                                 <Image source={{ uri: item.icon }} style={{ width: 50, height: 50 }} />
                             </View>
                             <Text style={{ color: theme['muted-white'] }}>{item.name}</Text>
-                            <Text style={{ color: theme['muted-white'] }}>{calculateDistanceCost(item.cost)}</Text>
+                            <Text style={{ color: theme['muted-white'] }}>{calculateDistanceCost(item.cost)} {"RWF"}</Text>
                         </View>
                     </View>
                 )}
